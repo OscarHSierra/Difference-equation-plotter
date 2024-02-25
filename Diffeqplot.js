fa1acaf6547854faa97825e5919dfd20cfb0eca5
OscarHSierra <110925231+OscarHSierra@users.noreply.github.com>
2024-02-25 16:52:54 -0500

    Add files via upload

--- Diffeqplot.js
+++ Diffeqplot.js
@@ -3,12 +3,9 @@ var b= document.getElementById("input_b");
 var c= document.getElementById("input_c");
 var d= document.getElementById("input_d");
 var e= document.getElementById("input_e");
-
-
-
+var Tm= document.getElementById("input_Tm");
 var boton=document.getElementById("boton");
 var controlador_js=document.getElementById("controlador");
-
 var b0=0.0;
 var r=document.getElementById("resultado");
 var im=document.getElementById("imagen_res");
@@ -24,22 +21,22 @@ var offset=0.0;
 var s=0.0;
 var num=0.0;
 var r=document.getElementById("resultado");
-
 var z=document.getElementById("dibujito"); //se obtiene el id desde el canvas
 var lienzo= z.getContext('2d'); //se define el dibujo en 2d
 lienzo.scale(1.3,1.3);
-
-
 var duty=350;
-
-
 
 function dibujar_linea()
 {
-
+    Tm.value=parseInt(Tm.value);
+    if (Tm.value>99 || Tm.value<=0)
+    {
+       alert("Error Tm debe ser mayor a cero y menor a 100uS");
+        Tm.value=99;
+    }
 
     w2=w.toFixed(2);
-    lienzo.clearRect(0, 0, 400, 380);       //Se borra el canvas
+    lienzo.clearRect(0, 0, 480, 380);       //Se borra el canvas
     lienzo.fillStyle = "rgb(62, 0, 168)";
 
 
@@ -54,33 +51,72 @@ function dibujar_linea()
 
     lienzo.beginPath(); 
     lienzo.moveTo(0,200);
-    while (contador < 50)
+
+    var tiempomuestreo=Tm.value;
+
+    if(Tm.value<5)
     {
-        x=1;
-       res=a.value*y2 + b.value*y1 + c.value*x2 + d.value*x1 + e.value*x;
-        //res=-0.6887*y2+1.39*y1 + 0.7119*x2 +1.424*x1 +0.7119*x;
-
-        lienzo.lineTo(contador*6,-res*10+200);
-        
-        if(aux==5|| contador==0)
+        while (contador < 300)
         {
-            lienzo.fillText( "x", contador*6, -res*10+202);
-            lienzo.fillText( res.toFixed(2), contador*6+3, -res*10+195);
-            lienzo.fillText("|", contador*6, 210);
-            lienzo.fillText("|", contador*6, 209);
-            lienzo.fillText( 1*contador+"uS", contador*6, 230);
-            aux=0
+
+    
+           
+           
+            
+            if(aux==Tm.value|| contador==0||aux==Tm.value*2||aux==Tm.value*3||aux==Tm.value*4||aux==Tm.value*5||aux==Tm.value*6)
+            {
+                x=1;
+                res=a.value*y2 + b.value*y1 + c.value*x2 + d.value*x1 + e.value*x;
+                 //res=-0.6887*y2+1.39*y1 + 0.7119*x2 +1.424*x1 +0.7119*x;
+                lienzo.lineTo(contador*(1/Tm.value)*28,-res*5+200);
+                lienzo.fillText( "x", contador*(1/Tm.value)*28, -res*5+202);
+                lienzo.fillText( res.toFixed(2), contador*(1/Tm.value)*28+3, -res*5+195);
+                lienzo.fillText("|", contador*(1/Tm.value)*28, 210);
+                lienzo.fillText("|", contador*(1/Tm.value)*28, 209);
+                lienzo.fillText( 1*contador+"uS", contador*(1/Tm.value)*28, 230);
+                aux=0
+                y2=y1;
+                y1=res;
+                x2=x1;
+                x1=x;
+            }
+            aux++;
+            contador++;
+            tiempomuestreo = tiempomuestreo*2;
+   
         }
-        aux++;
-        contador++;
-
-        y2=y1;
-        y1=res;
-        x2=x1;
-        x1=x;
-        
-
     }
+    else
+    {
+        while (contador < Tm.value*20)
+        {
+           
+            if(aux==Tm.value|| contador==0||aux==Tm.value*2||aux==Tm.value*3||aux==Tm.value*4)
+            {
+                x=1;
+                res=a.value*y2 + b.value*y1 + c.value*x2 + d.value*x1 + e.value*x;
+                 //res=-0.6887*y2+1.39*y1 + 0.7119*x2 +1.424*x1 +0.7119*x;
+                lienzo.lineTo(contador*(1/Tm.value)*32,-res*5+200);
+                lienzo.fillText( "x", contador*(1/Tm.value)*32, -res*5+202);
+                lienzo.fillText( res.toFixed(2), contador*(1/Tm.value)*32+3, -res*5+195);
+                lienzo.fillText("|", contador*(1/Tm.value)*32, 210);
+                lienzo.fillText("|", contador*(1/Tm.value)*32, 209);
+                lienzo.fillText( 1*contador+"uS", contador*(1/Tm.value)*32, 230);
+                aux=0
+                y2=y1;
+                y1=res;
+                x2=x1;
+                x1=x;
+            }
+            aux++;
+            contador++;
+            tiempomuestreo = tiempomuestreo*2;
+
+        }
+    }
+
+
+    
 
     lienzo.stroke();
     //lienzo.fill();
@@ -88,30 +124,8 @@ function dibujar_linea()
   //  lienzo.moveTo(0,250);
     lienzo.lineTo(0,100);
 
-   // lienzo.lineTo(duty,100);
-   // lienzo.lineTo(duty,250);
-   // lienzo.moveTo(0,250);
-
-  //  lienzo.stroke();
-   // lienzo.fill();
-    // lienzo.font = "60px Comic Sans MS";
-    // lienzo.fillStyle = "black";
-    // lienzo.textAlign = "center";
-    // ro=t.value.toString();
-    // lienzo.fillText("|", 300, 270);
-    // lienzo.font = "20px Comic Sans MS";
 
 
-    // s2=parseFloat(t.value);
-    // s3=s2.toFixed(2);
-    // s4=((ciclo.value*0.01*t.value).toFixed(2)).toString() ;
-    // lienzo.font = "60px Comic Sans MS";
-    // lienzo.fillText("|", duty, 100);
-    // lienzo.font = "20px Comic Sans MS";
-
-
-
-    // lienzo.fillText("Duty="+ s4+"seg", duty+offset, 30);
 }
    
 
